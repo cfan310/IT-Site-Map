6e7250f7ef5230a34d056754d50861afa52637c5
naffer <cfanning43@gmail.com>
2024-09-24 13:47:54 -0400

    removed table

--- floorplan2.py
+++ floorplan2.py
@@ -4,11 +4,9 @@ from tkinter import Tk, Frame, Canvas, Button, Toplevel, Label
 import sqlite3
  
 '''
-def create_table():
-  # lightweight in-app table to contain all of the lab object data 
-    # will allow for editing creation and deletion 
 
-    # tabel to be dynamically tested in sqlite then connected and mapped to MySQL db server using SQLAlchemy
+# rem table func
+
 '''
 
 # --------------Floor 12 Lab Dictionary Data---------------------------------------
